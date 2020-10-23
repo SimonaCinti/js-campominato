c2cf79070d2ab8aa45ed0f89a582c94022df94b6
Simona Cinti <simona.cinti2@gmail.com>
2020-10-23 15:19:38 +0200

    pc casual number

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,28 @@
-console.log("hello javascript");
+
+//TODO Il computer deve generare 16 numeri casuali (bombe) tra 1 e 100. I numeri non possono essere duplicati 
+
+// Variabili per calcolo numeri pc
+var minPc = 1;
+var maxPc = 30;
+var pcBomb = [];
+var count = 16;
+
+while (pcBomb.length < count) {
+    var pcNumber = random (minPc, maxPc);
+    if (! pcBomb.includes(pcNumber)) {
+        pcBomb.push(pcNumber);
+    }
+}
+
+console.log(pcBomb);
+
+/**
+ * Generazione numeri random
+ * @param {number} min  numero random minimo 
+ * @param {number} max  numero random massimo
+ */
+
+function random (min, max){
+    var risultato = Math.floor( Math.random() * (max - min +1) + min);
+    return risultato;
+}
